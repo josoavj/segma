fce2f18084375f2dd151e3398646b0f4f31bddca
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2026-08-03 19:14:30 +0300

    Create bounding_boxes_overlay.dart

---
+++ lib/widgets/image_viewer/bounding_boxes_overlay.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:segma/models/models.dart';
+
+class BoundingBoxesOverlay extends StatelessWidget {
+  final SegmentationResult segmentation;
+  final Size imageSize;
+
+  const BoundingBoxesOverlay({
+    super.key,
+    required this.segmentation,
+    required this.imageSize,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+      child: CustomPaint(
+        painter: BoundingBoxesPainter(
+          objects: segmentation.objects,
+          imageSize: imageSize,
+          imageWidth: segmentation.width,
+          imageHeight: segmentation.height,
+        ),
+      ),
+    );
+  }
+}
+
+class BoundingBoxesPainter extends CustomPainter {
+  final List<SegmentedObject> objects;
+  final Size imageSize;
+  final int imageWidth;
+  final int imageHeight;
+
+  BoundingBoxesPainter({
+    required this.objects,
+    required this.imageSize,
+    required this.imageWidth,
+    required this.imageHeight,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (imageWidth <= 0 || imageHeight <= 0) return;
+    
+    final imageAspectRatio = imageWidth / imageHeight;
+    final canvasAspectRatio = size.width / size.height;
+
+    late double scaleX;
+    late double scaleY;
+    late double offsetX;
+    late double offsetY;
+
+    if (imageAspectRatio > canvasAspectRatio) {
+      scaleX = size.width / imageWidth;
+      scaleY = scaleX;
+      offsetX = 0;
+      offsetY = (size.height - imageHeight * scaleY) / 2;
+    } else {
+      scaleY = size.height / imageHeight;
+      scaleX = scaleY;
+      offsetY = 0;
+      offsetX = (size.width - imageWidth * scaleX) / 2;
+    }
+
+    final borderPaint = Paint()
+      ..color = Colors.white
+      ..strokeWidth = 2.0
+      ..style = PaintingStyle.stroke;
+
+    final bgPaint = Paint()
+      ..color = Colors.black.withValues(alpha: 0.7)
+      ..style = PaintingStyle.fill;
+
+    final textPainter = TextPainter(textDirection: TextDirection.ltr);
+
+    for (final object in objects) {
+      final bbox = object.bbox;
+
+      final x1 = bbox.x1.toDouble();
+      final y1 = bbox.y1.toDouble();
+      final x2 = bbox.x2.toDouble();
+      final y2 = bbox.y2.toDouble();
+
+      final canvasX1 = offsetX + x1 * scaleX;
+      final canvasY1 = offsetY + y1 * scaleY;
+      final canvasX2 = offsetX + x2 * scaleX;
+      final canvasY2 = offsetY + y2 * scaleY;
+
+      canvas.drawRect(
+        Rect.fromLTRB(canvasX1, canvasY1, canvasX2, canvasY2),
+        borderPaint,
+      );
+
+      final label =
+          '${object.label} (${(object.confidence * 100).toStringAsFixed(0)}%)';
+
+      textPainter.text = TextSpan(
+        text: label,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 12,
+          fontWeight: FontWeight.bold,
+        ),
+      );
+      textPainter.layout();
+
+      final labelX = canvasX1;
+      final labelY = canvasY1 - textPainter.height - 4;
+
+      canvas.drawRect(
+        Rect.fromLTWH(
+          labelX - 4,
+          labelY,
+          textPainter.width + 8,
+          textPainter.height + 4,
+        ),
+        bgPaint,
+      );
+
+      textPainter.paint(canvas, Offset(labelX, labelY + 2));
+    }
+  }
+
+  @override
+  bool shouldRepaint(BoundingBoxesPainter oldDelegate) {
+    return objects.length != oldDelegate.objects.length ||
+        imageSize != oldDelegate.imageSize;
+  }
+}
