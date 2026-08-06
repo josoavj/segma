73ac5f2254d7231e391a2d6cf06961ffb9a07633
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2026-08-06 20:50:29 +0300

    fix(services): update notification service for Riverpod 3.x compatibility

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 import 'package:uuid/uuid.dart';
 import 'package:segma/models/notification_model.dart';
 
